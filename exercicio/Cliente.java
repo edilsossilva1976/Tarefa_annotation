e7ca98431d70eeb0e6ee189eeead3c9dc78bae3d
Edilson Alves Ferreira da Silva <139266985+edilsossilva1976@users.noreply.github.com>
2024-03-30 21:09:21 -0300

    upload arquivos annotation
    Upload referente ao exercicio do modulo 18 do curso de Backend Java da EBAC.

---
+++ exercicio/Cliente.java
@@ -0,0 +1,9 @@
+package br.com.anotacoes.exercicio;
+
+@Tabela(nome="clientes")
+public class Cliente {
+	
+	private Long id;
+	private String nome;
+
+}
---
+++ exercicio/Tabela.java
@@ -0,0 +1,15 @@
+package br.com.anotacoes.exercicio;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Inherited;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Retention(RetentionPolicy.RUNTIME)
+@Target(ElementType.TYPE)
+@Inherited
+
+public @interface Tabela {
+	String nome();
+}
